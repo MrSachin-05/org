ac3082275e2a31f30de6a807f56b157b9fcb92ea
MrSachin-05 <sachidanandapanigrahi96@gmail.com>
2026-08-26 09:57:26 +0530

    apperance

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant={"destructive"}>Welcome to My App ❤️ Mr.Sachin-05</Button>
+      <Button variant={"destructive"}>Welcome to My App❤️</Button>
     </div>
   );
 }
